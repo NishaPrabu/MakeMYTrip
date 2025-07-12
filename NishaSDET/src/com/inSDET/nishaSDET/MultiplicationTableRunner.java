974c575071e9c72d7f4a101be4dd1ae18aee6ca1
HP <HP@DESKTOP-HKRNLIP>
2025-07-12 07:11:05 +0530

    First commit

---
+++ NishaSDET/src/com/inSDET/nishaSDET/MultiplicationTableRunner.java
@@ -0,0 +1,12 @@
+package com.inSDET.nishaSDET;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.printMultiplication();
+		table.printMultiplication(4);
+		table.printMultiplication(4, 1, 10);
+	}
+
+}
---
+++ NishaSDET/src/com/inSDET/nishaSDET/SDETPrj1.java
@@ -0,0 +1,10 @@
+package com.inSDET.nishaSDET;
+
+public class SDETPrj1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First project");
+	}
+
+}
